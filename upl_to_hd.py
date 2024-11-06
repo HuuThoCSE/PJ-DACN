ad10d7d3655f6054264f66014cb45512b6302536
Thomas Nguyen <huutho.cse@gmail.com>
2024-11-06 09:22:37 +0000

    .

--- upl_to_hd.py
+++ upl_to_hd.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-import pydoop.hdfs as hdfs
+from hdfs import InsecureClient
 import os
 
 # Tạo SparkSession
@@ -8,7 +8,11 @@ spark = SparkSession.builder.appName("UploadImagesToHDFS").getOrCreate()
 # Đường dẫn cục bộ đến thư mục chứa ảnh
 local_dir = "/home/huuthocse/square_pick-2"
 # Đường dẫn HDFS nơi bạn muốn lưu ảnh
-hdfs_dir = "hdfs:///datasets/"  # Đảm bảo định dạng này
+hdfs_url = "http://namenode_host:50070"  # Địa chỉ WebHDFS
+hdfs_dir = "/datasets/"
+
+# Tạo client HDFS
+client = InsecureClient(hdfs_url, user='your_username')
 
 # Đọc toàn bộ file trong thư mục cục bộ
 image_rdd = spark.sparkContext.binaryFiles(f"file://{local_dir}/*")
@@ -18,10 +22,7 @@ def save_to_hdfs(file_data):
     file_path, file_content = file_data
     file_name = os.path.basename(file_path)  # Lấy tên file từ đường dẫn
     hdfs_file_path = os.path.join(hdfs_dir, file_name)  # Đường dẫn đầy đủ trên HDFS
-
-    # Sử dụng pydoop để ghi file vào HDFS
-    with hdfs.open(hdfs_file_path, "wb") as f:
-        f.write(file_content)
+    client.write(hdfs_file_path, file_content, overwrite=True)
 
 # Ghi từng file lên HDFS
 image_rdd.foreach(save_to_hdfs)
