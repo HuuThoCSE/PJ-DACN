8f19782b3729dc123e0f174b132010220e5ab5af
Thomas Nguyen <huutho.cse@gmail.com>
2024-11-06 09:23:39 +0000

    .

--- upl_to_hd.py
+++ upl_to_hd.py
@@ -8,24 +8,26 @@ spark = SparkSession.builder.appName("UploadImagesToHDFS").getOrCreate()
 # Đường dẫn cục bộ đến thư mục chứa ảnh
 local_dir = "/home/huuthocse/square_pick-2"
 # Đường dẫn HDFS nơi bạn muốn lưu ảnh
-hdfs_url = "http://namenode_host:50070"  # Địa chỉ WebHDFS
+hdfs_url = "http://namenode_host:50070"  # Thay bằng địa chỉ WebHDFS
 hdfs_dir = "/datasets/"
 
-# Tạo client HDFS
+# Tạo client HDFS bên ngoài hàm
 client = InsecureClient(hdfs_url, user='your_username')
 
 # Đọc toàn bộ file trong thư mục cục bộ
 image_rdd = spark.sparkContext.binaryFiles(f"file://{local_dir}/*")
 
 # Hàm để ghi file lên HDFS
-def save_to_hdfs(file_data):
+def save_to_hdfs(file_data, hdfs_dir):
     file_path, file_content = file_data
     file_name = os.path.basename(file_path)  # Lấy tên file từ đường dẫn
     hdfs_file_path = os.path.join(hdfs_dir, file_name)  # Đường dẫn đầy đủ trên HDFS
+    
+    # Ghi dữ liệu lên HDFS
     client.write(hdfs_file_path, file_content, overwrite=True)
 
-# Ghi từng file lên HDFS
-image_rdd.foreach(save_to_hdfs)
+# Sử dụng lambda để truyền đối số hdfs_dir cho mỗi phần tử RDD
+image_rdd.foreach(lambda file_data: save_to_hdfs(file_data, hdfs_dir))
 
 # Dừng SparkSession
 spark.stop()
