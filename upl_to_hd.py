788916b62659ae514571876d6e0ca742650cedbe
Thomas Nguyen <huutho.cse@gmail.com>
2024-11-06 09:26:03 +0000

    .

--- upl_to_hd.py
+++ upl_to_hd.py
@@ -7,27 +7,28 @@ spark = SparkSession.builder.appName("UploadImagesToHDFS").getOrCreate()
 
 # Đường dẫn cục bộ đến thư mục chứa ảnh
 local_dir = "/home/huuthocse/square_pick-2"
-# Đường dẫn HDFS nơi bạn muốn lưu ảnh
+# URL và thư mục đích HDFS
 hdfs_url = "http://namenode_host:50070"  # Thay bằng địa chỉ WebHDFS
 hdfs_dir = "/datasets/"
-
-# Tạo client HDFS bên ngoài hàm
-client = InsecureClient(hdfs_url, user='your_username')
 
 # Đọc toàn bộ file trong thư mục cục bộ
 image_rdd = spark.sparkContext.binaryFiles(f"file://{local_dir}/*")
 
 # Hàm để ghi file lên HDFS
-def save_to_hdfs(file_data, hdfs_dir):
+def save_to_hdfs(file_data, hdfs_url, hdfs_dir):
+    from hdfs import InsecureClient  # Khởi tạo `InsecureClient` bên trong hàm
     file_path, file_content = file_data
     file_name = os.path.basename(file_path)  # Lấy tên file từ đường dẫn
     hdfs_file_path = os.path.join(hdfs_dir, file_name)  # Đường dẫn đầy đủ trên HDFS
     
+    # Khởi tạo client HDFS trong mỗi worker
+    client = InsecureClient(hdfs_url, user='huuthocse')
+    
     # Ghi dữ liệu lên HDFS
     client.write(hdfs_file_path, file_content, overwrite=True)
 
-# Sử dụng lambda để truyền đối số hdfs_dir cho mỗi phần tử RDD
-image_rdd.foreach(lambda file_data: save_to_hdfs(file_data, hdfs_dir))
+# Sử dụng `lambda` để truyền thêm đối số `hdfs_url` và `hdfs_dir`
+image_rdd.foreach(lambda file_data: save_to_hdfs(file_data, hdfs_url, hdfs_dir))
 
 # Dừng SparkSession
 spark.stop()
